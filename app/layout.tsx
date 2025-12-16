43bbc85aeb3ad5523995076ac460fdaba463c247
HATUMA Charles <hatumacharles1@gmail.com>
2025-12-16 12:30:14 +0000

    feat: dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
       <body className={`font-sans antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
-          enableSystem={false}
+          defaultTheme="system"
+          enableSystem
           disableTransitionOnChange
         >
           {children}
